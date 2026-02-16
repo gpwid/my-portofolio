04b534f0274dac6679f35caa08778bce619a098e
Gusti Panji Widodo <gustipanji2006@gmail.com>
2026-02-16 16:14:03 +0700

    fix: Add development ID to Sanity configuration

--- portofolio-blog/sanity.cli.ts
+++ portofolio-blog/sanity.cli.ts
@@ -1,4 +1,4 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
@@ -6,6 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
+    appId: 'b6tp4oadxarj7k98cyvh2e3m',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
